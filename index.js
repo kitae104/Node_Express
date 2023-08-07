61ee65f4c2dfcf36f3363e838239cd9b9968ce16
Kitae's Github <aqua0405@gmail.com>
2023-08-07 18:19:59 +0900

    router

--- index.js
+++ index.js
@@ -44,14 +44,94 @@ app.get('/', (req, res) => {        //express 앱(app)을 넣고, root directory
     `<h2>${title}</h2>${description}
      <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
     `,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   res.send(html);
   
 });
 
+app.get('/topic/create', function(req, res){
+  
+  var title = 'WEB - create';
+  var list = template.list(req.list);
+  var html = template.HTML(title, list, `
+    <form action="/topic/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  res.send(html);
+  
+});
+
+app.post('/topic/create_process', function(req, res){
+  var post = req.body;          // body-parser를 이용해서, post 방식으로 전송된 데이터를, 원래의 데이터로 변환한다.
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){   
+    res.redirect(`/topic/${title}`);  //redirect를 이용해서, 사용자를 다른 페이지로 보낸다.
+  }); 
+}); 
+
+app.get('/topic/update/:pageId', function(req, res, next){
+  
+  var filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+
+    if(err){
+      next(err);
+    } else {
+      var title = req.params.pageId;
+      var list = template.list(req.list);
+      var html = template.HTML(title, list,
+        `
+        <form action="/topic/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+      );
+      res.send(html);
+    }      
+  });  
+});
+
+app.post('/topic/update_process', function(req, res){  
+  var post = req.body;        // body-parser를 이용해서, post 방식으로 전송된 데이터를, 원래의 데이터로 변환한다.
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      res.redirect(`/topic/${title}`)
+    })
+  });
+  
+});
+
+app.post('/topic/delete_process', function(req, res){  
+  var post = req.body;      // body-parser를 이용해서, post 방식으로 전송된 데이터를, 원래의 데이터로 변환한다.
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error){
+    res.redirect('/');
+  });  
+});
+
 // URL에 따라 다른 페이지를 보여주는 기능
-app.get('/page/:pageId', function(req, res, next){
+app.get('/topic/:pageId', function(req, res, next){
   
   let filteredId = path.parse(req.params.pageId).base;                    //파일 목록에서, 사용자가 요청한 페이지를 찾는다.
   fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){   //파일을 읽는다.
@@ -68,9 +148,9 @@ app.get('/page/:pageId', function(req, res, next){
       let list = template.list(req.list);
       let html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
+        ` <a href="/topic/create">create</a>
+          <a href="/topic/update/${sanitizedTitle}">update</a>
+          <form action="/topic/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
@@ -80,86 +160,12 @@ app.get('/page/:pageId', function(req, res, next){
   });  
 });
 
-app.get('/create', function(req, res){
-  
-  var title = 'WEB - create';
-  var list = template.list(req.list);
-  var html = template.HTML(title, list, `
-    <form action="/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  res.send(html);
-  
-});
 
-app.post('/create_process', function(req, res){
-  var post = req.body;          // body-parser를 이용해서, post 방식으로 전송된 데이터를, 원래의 데이터로 변환한다.
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    res.writeHead(302, {Location: `/?id=${title}`});
-    res.end();
-  }); 
-}); 
 
-app.get('/update/:pageId', function(req, res, next){
-  
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
 
-      if(err){
-        next(err);
-      } else {
-        var title = req.params.pageId;
-        var list = template.list(req.list);
-        var html = template.HTML(title, list,
-          `
-          <form action="/update_process" method="post">
-            <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-            <p>
-              <textarea name="description" placeholder="description">${description}</textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-          `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-        res.send(html);
-      }      
-    });  
-});
 
-app.post('/update_process', function(req, res){  
-  var post = req.body;        // body-parser를 이용해서, post 방식으로 전송된 데이터를, 원래의 데이터로 변환한다.
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function(error){
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      res.redirect(`/?id=${title}`)
-    })
-  });
-  
-});
 
-app.post('/delete_process', function(req, res){  
-  var post = req.body;      // body-parser를 이용해서, post 방식으로 전송된 데이터를, 원래의 데이터로 변환한다.
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function(error){
-    res.redirect('/');
-  });  
-});
+
 
 //==============================
 // 오류 처리
